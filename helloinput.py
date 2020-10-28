0c0ea3e4b219161cd37f5d1e88cb1c42d38c23d6
mcsilla <majoroscsilla88@gmail.com>
2020-10-28 12:58:16 +0100

    Modify helloinput.py

--- helloinput.py
+++ helloinput.py
@@ -1,1 +1,13 @@
-pass  # this Python keyword is just a placeholder, stands for "don't do anything"
+def get_hello_message():
+  name = input("What's your name?\n")
+  if name == "":
+    return "Hello, World!"
+  else:
+    return "Hello, " + name + "!"
+
+
+def say_hello(message):
+  print(message)
+
+
+say_hello(get_hello_message())
