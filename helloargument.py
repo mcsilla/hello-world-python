dd7f213f5cd061a7d90d68fb1c59d3768fe06058
mcsilla <majoroscsilla88@gmail.com>
2020-10-28 13:06:37 +0100

    Modify helloargument.py

--- helloargument.py
+++ helloargument.py
@@ -1,1 +1,17 @@
-pass  # this Python keyword is just a placeholder, stands for "don't do anything"
+def get_user_name():
+  name = input("What's your name?\n")
+  return name.capitalize()
+  
+
+def get_hello_message(name):
+  if name == "":
+    return "Hello, World!"
+  else:
+    return "Hello, " + name + "!"
+
+
+def say_hello(message):
+  print(message)
+
+
+say_hello(get_hello_message(get_user_name()))
